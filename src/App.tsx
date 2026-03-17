642a99c786a90edc47ec6646dde6d07e034eb44f
Arne Schüler <arne@schueler.dev>
2026-03-17 10:09:57 +0100

    style: add active states to navigation and update footer links

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,14 @@ function App() {
       <MainLayout>
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/statistics" element={<RecipeCatalog />} />
           <Route path="/exercises" element={<ExerciseLibrary />} />
           <Route path="/recipes" element={<RecipeCatalog />} />
+          <Route path="/settings" element={<RecipeCatalog />} />
+          <Route path="/legal-notice" element={<RecipeCatalog />} />
+          <Route path="/privacy-policy" element={<RecipeCatalog />} />
+          <Route path="/terms-conditions" element={<RecipeCatalog />} />
+          <Route path="/cancellation-policy" element={<RecipeCatalog />} />
         </Routes>
       </MainLayout>
     </Router>
--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -31,35 +31,48 @@ export default function MainLayout({ children }) {
           <aside className=" w-50 h-full text-white py-8">
             <nav className="flex flex-col h-full justify-between gap-12 p-4">
               <div className="flex flex-col gap-12">
-                <NavLink to="/" className="flex flex-col items-center">
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    `flex flex-col items-center ${isActive ? "text-blue-500 " : "text-white"}`
+                  }
+                >
                   <LayoutDashboard />
                   Dashboard
                 </NavLink>
                 <NavLink
-                  to="/exercises"
-                  className="flex gap-2 flex-col items-center"
+                  to="/statistics"
+                  className={({ isActive }) =>
+                    `flex flex-col items-center ${isActive ? "text-blue-500 " : "text-white"}`
+                  }
                 >
                   <ChartLine />
                   Statistics
                 </NavLink>
                 <NavLink
                   to="/exercises"
-                  className="flex flex-col gap-2 items-center"
+                  className={({ isActive }) =>
+                    `flex flex-col items-center ${isActive ? "text-blue-500 " : "text-white"}`
+                  }
                 >
                   <Dumbbell />
                   Exercises
                 </NavLink>
                 <NavLink
                   to="/recipes"
-                  className="flex gap-2 flex-col items-center"
+                  className={({ isActive }) =>
+                    `flex flex-col items-center ${isActive ? "text-blue-500 " : "text-white"}`
+                  }
                 >
                   <Utensils />
                   Recipes
                 </NavLink>
               </div>
               <NavLink
-                to="/recipes"
-                className="flex gap-2 flex-col items-center"
+                to="/settings"
+                className={({ isActive }) =>
+                  `flex flex-col items-center ${isActive ? "text-blue-500 " : "text-white"}`
+                }
               >
                 <Settings />
                 Settings
@@ -73,10 +86,30 @@ export default function MainLayout({ children }) {
       </div>
       <footer className="flex w-full justify-center">
         <nav className="flex gap-8  text-white p-4">
-          <NavLink to="/">Legal Notice</NavLink>
-          <NavLink to="/exercises">Privacy Policy</NavLink>
-          <NavLink to="/recipes">Terms & Conditions</NavLink>
-          <NavLink to="/recipes">Cancellation Policy</NavLink>
+          <NavLink
+            to="/legal-notice"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Legal Notice
+          </NavLink>
+          <NavLink
+            to="/privacy-policy"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Privacy Policy
+          </NavLink>
+          <NavLink
+            to="/terms-conditions"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Terms & Conditions
+          </NavLink>
+          <NavLink
+            to="/cancellation-policy"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Cancellation Policy
+          </NavLink>
         </nav>
       </footer>
     </div>
